27608ff3796ffd73ba9579d9d1a723535f490a18
Humbe1406 <humbe1406@gmail.com>
2025-07-04 13:35:01 -0400

    first commit

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,15 @@
+import { BookmarkIcon } from "@radix-ui/react-icons";
+import { Button } from "@radix-ui/themes";
+
+function LoginPage() {
+    return (
+        <div>
+            LoginPage
+            <Button>
+                <BookmarkIcon /> Login
+                </Button>
+        </div>
+    )
+}
+
+export default LoginPage
